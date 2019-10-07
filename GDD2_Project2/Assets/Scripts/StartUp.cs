ac2dd5dad9d914f60cfae60ad8069c2f20464dc5
Kyler McQuillan (RIT Student) <kjm7088@ad.rit.edu>
2019-10-07 10:52:38 -0400

    StartUp script editing and set up

--- GDD2_Project2/Assets/Scripts/StartUp.cs
+++ GDD2_Project2/Assets/Scripts/StartUp.cs
@@ -6,25 +6,43 @@ public class StartUp : MonoBehaviour
 {
     #region Variables
     // Instantiating Variables
-    double[] startUpX;
-    double[] startUpY;
+    private float[] startUpX;
+    private float[] startUpY;
+    private GameObject[,] starterTiles;
+    public float tileDifferential;
+
+    public GameObject templateSquare;
 
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-        // setting up matrix values
-        startUpX = new double[15];
-        startUpY = new double[7];
-
-        // inputting base values for matrix
-
+        TileStartUp();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// Sets up base variables for tiling
+    /// </summary>
+    void TileStartUp()
+    {
+        // setting up matrix values
+        startUpX = new float[15];
+        startUpY = new float[7];
+        starterTiles = new GameObject[15, 7];
+        if (tileDifferential == 0)
+            tileDifferential = 1.05f;
+
+        // inputting base values for matrix
+        for (int i = 0; i < startUpX.Length - 1; i++)
+        {
+
+        }
     }
 }
