2c88862fa3d5c547a91fa5846fc6b35e96160331
Kyler McQuillan (RIT Student) <kjm7088@ad.rit.edu>
2019-10-07 10:40:17 -0400

    Creating base matrix for checking

---
+++ GDD2_Project2/Assets/Scripts/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartUp : MonoBehaviour
+{
+    #region Variables
+    // Instantiating Variables
+    double[] startUpX;
+    double[] startUpY;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // setting up matrix values
+        startUpX = new double[15];
+        startUpY = new double[7];
+
+        // inputting base values for matrix
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
