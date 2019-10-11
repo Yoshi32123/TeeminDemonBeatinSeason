4f82eb7d2186e60c747b62af6bb78119edde96f4
Alex Sarnese (RIT Student) <ajs2877@ad.rit.edu>
2019-10-11 10:51:19 -0400

    title screen buttons functional. Working on options menu now

---
+++ GDD2_Project2/Assets/Scripts/PreviousScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PreviousScene : MonoBehaviour
+{
+    //
+    public void GoToPreviousScene()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetString("previousScene"));
+    }
+}
