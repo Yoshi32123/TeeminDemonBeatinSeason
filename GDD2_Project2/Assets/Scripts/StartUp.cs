f9dda2689fb3da6407417f5976736d6f0fe62332
Kyler McQuillan (RIT Student) <kjm7088@ad.rit.edu>
2019-10-11 10:58:19 -0400

    More work on base 2D array setup

--- GDD2_Project2/Assets/Scripts/StartUp.cs
+++ GDD2_Project2/Assets/Scripts/StartUp.cs
@@ -6,9 +6,14 @@ public class StartUp : MonoBehaviour
 {
     #region Variables
     // Instantiating Variables
+    public int xTiles;
+    public int yTiles;
     private float[] startUpX;
     private float[] startUpY;
     private GameObject[,] starterTiles;
+
+    public float TopLeftX;
+    public float TopLeftY;
     public float tileDifferential;
 
     public GameObject templateSquare;
@@ -24,25 +29,51 @@ public class StartUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ChangeColor(starterTiles[1, 1]);
     }
 
     /// <summary>
     /// Sets up base variables for tiling
     /// </summary>
-    void TileStartUp()
+    public void TileStartUp()
     {
         // setting up matrix values
-        startUpX = new float[15];
-        startUpY = new float[7];
-        starterTiles = new GameObject[15, 7];
-        if (tileDifferential == 0)
+        startUpX = new float[xTiles];
+        startUpY = new float[yTiles];
+        starterTiles = new GameObject[xTiles, yTiles];
+        if (tileDifferential == 0.0f)
             tileDifferential = 1.05f;
 
         // inputting base values for matrix
-        for (int i = 0; i < startUpX.Length - 1; i++)
+        for (int i = 0; i < startUpX.Length; i++)
         {
-
+            startUpX[i] = TopLeftX + (tileDifferential * i);
         }
+        for (int i = 0; i < startUpY.Length; i++)
+        {
+            startUpY[i] = TopLeftY - (tileDifferential * i);
+        }
+
+        // instantiating objects and adding to 2D array
+        for (int i = 0; i < startUpX.Length; i++)
+        {
+            for (int j = 0; j < startUpY.Length; j++)
+            {
+                starterTiles[i,j] = Instantiate(templateSquare, new Vector2(startUpX[i], startUpY[j]), Quaternion.identity);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Changes the color of the object that was clicked on
+    /// </summary>
+    /// <param name="clickedObject"> The object that was clicked on </param>
+    public void ChangeColor(GameObject clickedObject)
+    {
+        // gets the renderer for the object
+        Renderer changer = clickedObject.GetComponent<Renderer>();
+
+        // changes the material color
+        changer.material.SetColor("_Color", Color.green);
     }
 }
