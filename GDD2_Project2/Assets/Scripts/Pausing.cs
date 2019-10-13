0f0d15dc32266b91232a02180aa2c9e82899d7ad
Alex Sarnese (RIT Student) <ajs2877@ad.rit.edu>
2019-10-13 15:47:17 -0400

    Continuing menu connectivity

---
+++ GDD2_Project2/Assets/Scripts/Pausing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelData : MonoBehaviour
+{
+    static bool Paused = false;
+
+    ///<summary>alternates the paused state</summary>
+    public void TogglePausing()
+    {
+        Paused = !Paused;
+        if (Paused)
+        {
+
+        }
+        else
+        {
+            
+        }
+    }
+
+
+}
