a9891fb2b72f81bb5aa21c56eeb1386cc161c178
Kyler McQuillan (RIT Student) <kjm7088@ad.rit.edu>
2019-10-13 15:41:20 -0400

    Created a level1 scene and switched name of startUp.cs to TileManager

--- GDD2_Project2/Assets/Scripts/TileManager.cs
+++ GDD2_Project2/Assets/Scripts/TileManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartUp : MonoBehaviour
+public class TileManager : MonoBehaviour
 {
     #region Variables
     // Instantiating Variables
