1ac9c7d1ef30c0650aaca3335612c2fd91931198
Kyler McQuillan (RIT Student) <kjm7088@ad.rit.edu>
2019-10-13 15:32:52 -0400

    Path can now be created
    in sampleScene using StartUp.cs

--- GDD2_Project2/Assets/Scripts/StartUp.cs
+++ GDD2_Project2/Assets/Scripts/StartUp.cs
@@ -12,6 +12,8 @@ public class StartUp : MonoBehaviour
     private float[] startUpY;
     private GameObject[,] starterTiles;
 
+    private List<GameObject> finalPath;
+
     public float TopLeftX;
     public float TopLeftY;
     public float tileDifferential;
@@ -22,18 +24,34 @@ public class StartUp : MonoBehaviour
     public Sprite blueSprite;
     public Sprite greenSprite;
 
+    private GameObject start;
+    private GameObject end;
+    private GameObject lastTileClicked;
+
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
         TileStartUp();
+
+        // setting up the start and end tiles
+        start = Instantiate(templateSquare, new Vector2(starterTiles[0,1].transform.position.x - tileDifferential, starterTiles[0, 1].transform.position.y), Quaternion.identity);
+        ChangeColor(start, greenSprite);
+        //finalPath.Add(start);
+        end = Instantiate(templateSquare, new Vector2(starterTiles[xTiles-1, yTiles-2].transform.position.x + tileDifferential, starterTiles[xTiles - 1, yTiles - 2].transform.position.y), Quaternion.identity);
+        ChangeColor(end, greenSprite);
+
+        // setting first choice available
+        ChangeColor(starterTiles[0, 1], greenSprite);
+        lastTileClicked = starterTiles[0, 1];
     }
 
     // Update is called once per frame
     void Update()
     {
         MouseClickCheck();
+        NextPathChoices();
     }
 
     /// <summary>
@@ -72,10 +90,10 @@ public class StartUp : MonoBehaviour
     /// Changes the color of the object that was clicked on
     /// </summary>
     /// <param name="clickedObject"> The object that was clicked on </param>
-    public void ChangeColor(GameObject clickedObject)
+    public void ChangeColor(GameObject clickedObject, Sprite sprite)
     {
         // gets sprite renderer and changes the sprite
-        clickedObject.GetComponent<SpriteRenderer>().sprite = greenSprite;
+        clickedObject.GetComponent<SpriteRenderer>().sprite = sprite;
     }
 
     /// <summary>
@@ -85,14 +103,81 @@ public class StartUp : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
-            if (Physics.Raycast(ray, out hit, 100))
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            if (hit.collider != null)
             {
-                Debug.DrawLine(ray.origin, hit.point);
-                Debug.Log("Hit object: " + hit.collider.gameObject.name);
-                ChangeColor(hit.collider.gameObject);
+                if (ValidPath(hit.collider.gameObject))
+                {
+                    lastTileClicked = hit.collider.gameObject;
+                    ChangeColor(hit.collider.gameObject, greenSprite);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Determines if the clicked cell is valid
+    /// </summary>
+    /// <param name="clickedObject">The object that was clicked on</param>
+    /// <returns></returns>
+    public bool ValidPath(GameObject clickedObject)
+    {
+        if (clickedObject.GetComponent<SpriteRenderer>().sprite == blueSprite)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Will set the cells available for the next path choice
+    /// </summary>
+    public void NextPathChoices()
+    {
+        int xIndex = -1;
+        int yIndex = -1;
+
+        for (int i = 0; i < startUpX.Length; i++)
+        {
+            for (int j = 0; j < startUpY.Length; j++)
+            {
+                if (lastTileClicked == starterTiles[i, j])
+                {
+                    xIndex = i;
+                    yIndex = j;
+                }
+            }
+        }
+
+        for (int i = 0; i < startUpX.Length; i++)
+        {
+            for (int j = 0; j < startUpY.Length; j++)
+            {
+                if (starterTiles[i, j].GetComponent<SpriteRenderer>().sprite == greenSprite || starterTiles[i, j] == lastTileClicked)
+                {
+                    // do nothing
+                }
+                else if (i == xIndex + 1 && j == yIndex || i == xIndex - 1 && j == yIndex || i == xIndex && j == yIndex + 1 || i == xIndex && j == yIndex - 1)
+                {
+                    // checks if cell is tower
+                    if (/*cell is tower*/false)
+                    {
+
+                    }
+                    else
+                    {
+                        starterTiles[i, j].GetComponent<SpriteRenderer>().sprite = blueSprite;
+                    }
+                }
+                else
+                {
+                    starterTiles[i, j].GetComponent<SpriteRenderer>().sprite = redSprite;
+                }
             }
         }
     }
