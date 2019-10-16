5d44571dabb92e50ea941d38cd84f75dfca0a4a4
Alex Sarnese (RIT Student) <ajs2877@ad.rit.edu>
2019-10-16 10:53:18 -0400

    working on brightness slider

---
+++ GDD2_Project2/Assets/Scripts/BrightnessFunctonality.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrightnessFunctonality : MonoBehaviour
+{
+    public float GammaCorrection;
+
+
+    void Update()
+    {
+        
+        RenderSettings.ambientLight = new Color(GammaCorrection, GammaCorrection, GammaCorrection, 1.0f);
+    }
+
+    public void OnValueChanged(float newValue)
+    {
+        GammaCorrection = newValue;
+    }
+}
