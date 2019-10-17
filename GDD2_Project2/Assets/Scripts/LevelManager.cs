b0e814742caa51ed51ab995648ac2dc228dd3693
Nick Rasmussen (RIT Student) <nlr3026@ad.rit.edu>
2019-10-17 14:34:43 -0400

    score/star system

--- GDD2_Project2/Assets/Scripts/LevelManager.cs
+++ GDD2_Project2/Assets/Scripts/LevelManager.cs
@@ -5,25 +5,46 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     List<GameObject> enemies = new List<GameObject>();
+
+    [Header("Enemies")]
     [SerializeField] GameObject pref_Enemy = null;
+    [SerializeField] float timePerSpawn = 0.0f;
+    [SerializeField] int numberOfEnemies = 0; //how many enemies in total for this level?
+
+    [Header("Health")]
+    [SerializeField] int maxHealth = 100;
+
+    [Header("Score")]
+    [SerializeField] bool autoGenStarTresholds;
+    [SerializeField] int[] starThresholds;
 
     TileManager tileManager;
 
     float time;
-    [SerializeField] float timePerSpawn = 0.0f;
-
-    [SerializeField] int numberOfEnemies = 0; //how many enemies in total for this level?
     int currentEnemies = 0; //how many enemies have we spawned at any given time
 
-    int score = 0;
-    public void AddScore(int value) { score += value; }
-    [SerializeField] int[] starThresholds = new int[3]; //how many points are needed to earn each star
+    int score;
+    int stars = 0;
+
+    int health = 0;
+    public void TakeDamage(int damage) { health -= damage; }
 
     // Start is called before the first frame update
     void Start()
     {
         time = 0;
+        health = maxHealth;
         tileManager = GameObject.FindGameObjectWithTag("TileManager").GetComponent<TileManager>();
+
+        if (autoGenStarTresholds)
+        {
+            starThresholds = new int[3];
+            starThresholds[0] = (int)(numberOfEnemies * Enemy.scorePerKill * 0.70f);
+            starThresholds[1] = (int)(numberOfEnemies * Enemy.scorePerKill * 0.85f);
+            starThresholds[2] = numberOfEnemies * Enemy.scorePerKill;
+
+            Debug.Log(starThresholds[0] + "," + starThresholds[1] + "," + starThresholds[2]);
+        }
     }
 
     // Update is called once per frame
@@ -45,6 +66,45 @@ public class LevelManager : MonoBehaviour
                 time = timePerSpawn;
             }
 
+            foreach (GameObject enemy in enemies)
+            {
+                if (enemy.GetComponent<Enemy>().GetHasReachedEnd())
+                {
+                    enemy.GetComponent<Enemy>().health = 0;
+                    health--;
+                    Debug.Log("health: " + health);
+                }
+                else if(enemy.GetComponent<Enemy>().health <= 0)
+                {
+                    score += Enemy.scorePerKill;
+                }
+            }
+
+            if(enemies.Count == 0)
+            {
+                if(score >= starThresholds[2])
+                {
+                    //3 stars
+                    stars = 3;
+                }
+                else if(score >= starThresholds[1])
+                {
+                    //2 stars
+                    stars = 2;
+                }
+                else if(score >= starThresholds[0])
+                {
+                    //1 star
+                    stars = 1;
+                }
+                else
+                {
+                    //no stars
+                    stars = 0;
+                }
+                Debug.Log("Score: " + score);
+                Debug.Log("Stars: " + stars);
+            }
         }
         ClearDeadEnemies();
     }
@@ -53,7 +113,7 @@ public class LevelManager : MonoBehaviour
     {
         for(int i = enemies.Count - 1; i >= 0; i--)
         {
-            if(enemies[i].GetComponent<Enemy>().GetHealth() <= 0)
+            if(enemies[i].GetComponent<Enemy>().health <= 0)
             {
                 Destroy(enemies[i]);
                 enemies.RemoveAt(i);
@@ -82,4 +142,6 @@ public class LevelManager : MonoBehaviour
         }
         return enemies[closestIndex];
     }
+
+    public void AddScore(int value) { score += value; }
 }
