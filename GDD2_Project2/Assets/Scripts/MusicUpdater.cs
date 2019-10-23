ce2a32f0b9402fb469d172af5d6020bc4de0a2a2
alex sarnese <alexthepokemon@Gmail.com>
2019-10-22 20:15:45 -0400

    Option's music toggle now works

---
+++ GDD2_Project2/Assets/Scripts/MusicUpdater.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicUpdater : MonoBehaviour
+{
+    public GameObject demonsComing;
+    public GameObject creatingPath;
+    private AudioSource demonsComingAS;
+    private AudioSource creatingPathAS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        demonsComingAS = demonsComing.GetComponent<AudioSource>();
+        creatingPathAS = creatingPath.GetComponent<AudioSource>();
+
+        demonsComingAS.mute = !MusicFunctionality.MusicEnabled;
+        creatingPathAS.mute = !MusicFunctionality.MusicEnabled;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        demonsComingAS.mute = !MusicFunctionality.MusicEnabled;
+        creatingPathAS.mute = !MusicFunctionality.MusicEnabled;
+    }
+}
