a5812e322edabee81d610ba9493df2cc29accaf7
Kyler McQuillan (RIT Student) <kjm7088@ad.rit.edu>
2019-10-25 10:33:58 -0400

    added fireball prefab and script for animation

--- GDD2_Project2/Assets/Scripts/Fireball.cs
+++ GDD2_Project2/Assets/Scripts/Fireball.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
+    [Header("Sprites")]
+    [SerializeField] Sprite fireball1;
+    [SerializeField] Sprite fireball2;
+    [SerializeField] Sprite fireball3;
+
+    private int count;
+    private int spriteCount;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        count = 0;
+        spriteCount = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        count++;
+
+        if (count > 5)
+        {
+            count = 0;
+            spriteCount++;
+
+            if (spriteCount == 4)
+            {
+                spriteCount = 1;
+            }
+
+            ChangeSprite();
+        }
+    }
+
+    /// <summary>
+    /// Changes the sprite of the fireball. Loops through the different sprites
+    /// </summary>
+    public void ChangeSprite()
+    {
+        if (spriteCount == 1)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = fireball1;
+        }
+        else if (spriteCount == 2)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = fireball2;
+        }
+        else if (spriteCount == 3)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = fireball3;
+        }
     }
 }
