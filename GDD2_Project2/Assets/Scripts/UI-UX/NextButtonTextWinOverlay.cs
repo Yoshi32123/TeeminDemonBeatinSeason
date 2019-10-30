46dadacef2e86c6bb7cbd0f8a273be80c4f65452
alex sarnese <alexthepokemon@Gmail.com>
2019-10-30 17:13:12 -0400

    next stage button syas "credits" on the win screen of stage 10 now

---
+++ GDD2_Project2/Assets/Scripts/UI-UX/NextButtonTextWinOverlay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NextButtonTextWinOverlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (TowerBuilder.currentLevel == 10)
+        {
+            GetComponent<Text>().text = "Credits";
+        }
+    }
+
+}
